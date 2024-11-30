802a0e9de3fa1cf012136aba827370456b7fcc97
asdfish <lgray3420@gmail.com>
2024-11-30 12:08:20 -0500

    added autoplaying songs

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,14 @@ fn main() {
     let mut rs_player: RsPlayer = RsPlayer::new().unwrap();
     let mut event_handler: EventHandler = EventHandler::new();
 
+    rs_player.switch_song_to(0);
     while rs_player.running {
         let _ = rs_player.draw();
         let _ = event_handler.update(&mut rs_player);
+
+        if !rs_player.audio_handler.is_playing() {
+            rs_player.switch_song();
+        }
     }
 
     RsPlayer::uninit();
